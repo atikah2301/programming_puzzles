b769841ae06efe7b8590e8b810dafc8ab2175388
Atikah <atikah.hussain@hotmail.co.uk>
2022-03-07 17:46:01 +0000

    Create LeetCode_champagne_tower.py

---
+++ LeetCode/LeetCode_champagne_tower.py
@@ -0,0 +1,10 @@
+# Medium difficulty
+
+# Glasses are stacked in rows, in a triangle structure
+# Each glass holds 1 cup of champagne
+# There is 1 glass in the first row, 2 glasses in the second row etc.
+# n cups of champagne are poured into the top glass
+# Once filled, the excess champagne fill the glasses beneath
+
+# i = row:  0, 1, .., 99
+# j = glass: 0, 1, .., i
